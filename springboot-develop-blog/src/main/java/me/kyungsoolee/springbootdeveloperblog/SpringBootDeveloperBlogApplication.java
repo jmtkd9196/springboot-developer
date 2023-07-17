d54facb02a3616bd5699c347e74939bca5e6f0bf
KyungsooLee <jmtkd9196@gmail.com>
2023-07-17 22:44:16 +0900

    Init blog project

---
+++ springboot-develop-blog/src/main/java/me/kyungsoolee/springbootdeveloperblog/SpringBootDeveloperBlogApplication.java
@@ -0,0 +1,11 @@
+package me.kyungsoolee.springbootdeveloperblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDeveloperBlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDeveloperBlogApplication.class, args);
+    }
+}
