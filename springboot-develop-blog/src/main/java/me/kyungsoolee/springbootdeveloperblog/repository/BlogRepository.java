9136323b301a97914e8e5be9e235c7754c5ecb37
KyungsooLee <jmtkd9196@gmail.com>
2023-07-18 00:34:48 +0900

    Create BlogRepository

---
+++ springboot-develop-blog/src/main/java/me/kyungsoolee/springbootdeveloperblog/repository/BlogRepository.java
@@ -0,0 +1,7 @@
+package me.kyungsoolee.springbootdeveloperblog.repository;
+
+import me.kyungsoolee.springbootdeveloperblog.domain.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BlogRepository extends JpaRepository<Article, Long> {
+}
