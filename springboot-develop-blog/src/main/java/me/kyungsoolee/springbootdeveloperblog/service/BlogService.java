e2b0a7ffb6d2d64d3dcbb10f80f70136bf7b12b8
KyungsooLee <jmtkd9196@gmail.com>
2023-07-25 00:39:42 +0900

    Implement findAll() in BlogService

--- springboot-develop-blog/src/main/java/me/kyungsoolee/springbootdeveloperblog/service/BlogService.java
+++ springboot-develop-blog/src/main/java/me/kyungsoolee/springbootdeveloperblog/service/BlogService.java
@@ -6,6 +6,8 @@ import me.kyungsoolee.springbootdeveloperblog.dto.AddArticleRequest;
 import me.kyungsoolee.springbootdeveloperblog.repository.BlogRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor    // final이 붙거나 @NotNull이 붙은 필드의 생성자 추가
 @Service    // 빈으로 등록
 public class BlogService {
@@ -15,5 +17,10 @@ public class BlogService {
     // 블로그 글 추가 메서드
     public Article save(AddArticleRequest request) {
         return blogRepository.save(request.toEntity());
+    }
+
+    // 데이터베이스 글 모두 가져오는 메서드
+    public List<Article> findAll() {
+        return blogRepository.findAll();
     }
 }
