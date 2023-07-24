4c4bef454c43639ca02ccbccd9083683a7b0a96d
KyungsooLee <jmtkd9196@gmail.com>
2023-07-25 02:12:26 +0900

    Implement BlogApiControllerTest

--- springboot-develop-blog/src/main/java/me/kyungsoolee/springbootdeveloperblog/service/BlogService.java
+++ springboot-develop-blog/src/main/java/me/kyungsoolee/springbootdeveloperblog/service/BlogService.java
@@ -23,4 +23,6 @@ public class BlogService {
     public List<Article> findAll() {
         return blogRepository.findAll();
     }
+
+    
 }
--- springboot-develop-blog/src/test/java/me/kyungsoolee/springbootdeveloperblog/controller/BlogApiControllerTest.java
+++ springboot-develop-blog/src/test/java/me/kyungsoolee/springbootdeveloperblog/controller/BlogApiControllerTest.java
@@ -48,33 +48,33 @@ class BlogApiControllerTest {
         blogRepository.deleteAll();
     }
 
-    @DisplayName("addArticle: 블로그 글 추가에 성공한다.")
-    @Test
-    public void addArticle() throws Exception {
-        // given
-        final String url = "/api/articles";
-        final String title = "title";
-        final String content = "content";
-        final AddArticleRequest userRequest = new AddArticleRequest(title, content);
-
-        // 객체 JSON으로 직렬화
-        final String requestBody = objectMapper.writeValueAsString(userRequest);
-
-        // when
-        // 설정한 내용을 바탕으로 요청 전송
-        ResultActions result = mockMvc.perform(post(url)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(requestBody));
-
-        // then
-        result.andExpect(status().isCreated());
-
-        List<Article> articles = blogRepository.findAll();
-
-        assertThat(articles.size()).isEqualTo(1);
-        assertThat(articles.get(0).getTitle()).isEqualTo(title);
-        assertThat(articles.get(0).getContent()).isEqualTo(content);
-    }
+//    @DisplayName("addArticle: 블로그 글 추가에 성공한다.")
+//    @Test
+//    public void addArticle() throws Exception {
+//        // given
+//        final String url = "/api/articles";
+//        final String title = "title";
+//        final String content = "content";
+//        final AddArticleRequest userRequest = new AddArticleRequest(title, content);
+//
+//        // 객체 JSON으로 직렬화
+//        final String requestBody = objectMapper.writeValueAsString(userRequest);
+//
+//        // when
+//        // 설정한 내용을 바탕으로 요청 전송
+//        ResultActions result = mockMvc.perform(post(url)
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .content(requestBody));
+//
+//        // then
+//        result.andExpect(status().isCreated());
+//
+//        List<Article> articles = blogRepository.findAll();
+//
+//        assertThat(articles.size()).isEqualTo(1);
+//        assertThat(articles.get(0).getTitle()).isEqualTo(title);
+//        assertThat(articles.get(0).getContent()).isEqualTo(content);
+//    }
 
 
 
