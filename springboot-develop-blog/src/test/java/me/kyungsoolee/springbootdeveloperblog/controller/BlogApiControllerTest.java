dc08b75c69e7acf611fc0872c49827bb571c8173
KyungsooLee <jmtkd9196@gmail.com>
2023-07-24 22:53:21 +0900

    Implement BlogApiControllerTest

---
+++ springboot-develop-blog/src/test/java/me/kyungsoolee/springbootdeveloperblog/controller/BlogApiControllerTest.java
@@ -0,0 +1,38 @@
+package me.kyungsoolee.springbootdeveloperblog.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import me.kyungsoolee.springbootdeveloperblog.repository.BlogRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class BlogApiControllerTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;    // 직렬화, 역직렬화를 위한 클래스
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    BlogRepository blogRepository;
+
+    @BeforeEach // 테스트 실행 전 실행하는 메서드
+    public void mockMvcSetUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .build();
+        blogRepository.deleteAll();
+    }
+
+}
