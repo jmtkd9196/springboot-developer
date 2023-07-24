cc4fc7be18bedb9028fcf70e2eef8100c8b80e4a
KyungsooLee <jmtkd9196@gmail.com>
2023-07-25 00:43:40 +0900

    Implement ArticleResponse

---
+++ springboot-develop-blog/src/main/java/me/kyungsoolee/springbootdeveloperblog/dto/ArticleResponse.java
@@ -0,0 +1,18 @@
+package me.kyungsoolee.springbootdeveloperblog.dto;
+
+import lombok.Getter;
+import me.kyungsoolee.springbootdeveloperblog.domain.Article;
+
+@Getter
+public class ArticleResponse {
+
+    private final String title;
+    private final String content;
+
+    public ArticleResponse(Article article) {
+        this.title = article.getTitle();
+        this.content = article.getContent();
+    }
+
+
+}
