b4d55bf85d8a305553a70ab3b4d657f36d970358
KyungsooLee <jmtkd9196@gmail.com>
2023-07-25 00:58:46 +0900

    Implement findAllArticles() in BlogApiController

--- springboot-develop-blog/src/main/java/me/kyungsoolee/springbootdeveloperblog/controller/BlogApiController.java
+++ springboot-develop-blog/src/main/java/me/kyungsoolee/springbootdeveloperblog/controller/BlogApiController.java
@@ -3,12 +3,16 @@ package me.kyungsoolee.springbootdeveloperblog.controller;
 import lombok.RequiredArgsConstructor;
 import me.kyungsoolee.springbootdeveloperblog.domain.Article;
 import me.kyungsoolee.springbootdeveloperblog.dto.AddArticleRequest;
+import me.kyungsoolee.springbootdeveloperblog.dto.ArticleResponse;
 import me.kyungsoolee.springbootdeveloperblog.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController // HTTP Response Body에 객체 데이터를 JSON 형식으로 반환하는 컨트롤러
@@ -27,6 +31,14 @@ public class BlogApiController {
                 .body(savedArticle);
     }
 
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
 
-
+        return ResponseEntity.ok()
+                .body(articles);
+    }
 }
