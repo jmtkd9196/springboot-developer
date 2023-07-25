2f5a0568576ade0aa46edd83723b7a39979a676e
KyungsooLee <jmtkd9196@gmail.com>
2023-07-25 20:52:14 +0900

    Implement UserApiController

--- springboot-develop-blog/src/main/java/me/kyungsoolee/springbootdeveloperblog/controller/UserApiController.java
+++ springboot-develop-blog/src/main/java/me/kyungsoolee/springbootdeveloperblog/controller/UserApiController.java
@@ -1,9 +1,14 @@
 package me.kyungsoolee.springbootdeveloperblog.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import me.kyungsoolee.springbootdeveloperblog.dto.AddUserRequest;
 import me.kyungsoolee.springbootdeveloperblog.service.UserService;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
@@ -16,5 +21,11 @@ public class UserApiController {
     public String signup(AddUserRequest request) {
         userService.save(request);  // 회원 가입 메서드 호출
         return "redirection:/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
+        return "redirect:/login";
     }
 }
